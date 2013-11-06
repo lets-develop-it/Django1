b9c9e046660f9aff01d06c148d35b68132d33e33
Matthew James Karas <matthew.james.karas@gmail.com>
2013-11-05 22:00:31 -0500

    Added models
    Filling out the models and added the summer_reading app to the settings.

--- mysite/summer_reading/models.py
+++ mysite/summer_reading/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=50)
+    
+    def __unicode__(self):
+        return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(Book)
+    reviewer = models.ForeignKey(User)
+    review = models.TextField()
+
+    def __unicode__(self):
+        return ":".join([self.book.title, self.reviewer.username])
